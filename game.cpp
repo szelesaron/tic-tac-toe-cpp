174bf5e23c993c977b5635f9a6455a5fa0c6a25a
szelesaron <szelesaron@DESKTOP-B4MSSS9.localdomain>
2021-08-25 18:57:06 +0200

    finished drawing

--- game.cpp
+++ game.cpp
@@ -1,22 +1,67 @@
 #include<stdio.h>
 #include<iostream>
+#include <chrono>
+#include <thread>
 using namespace std;
 
 //constant
 bool game_over = false;
 const int dimension = 3;
+const int box_size = 12;
 int * arr  = new int[dimension * dimension];
 
 
 
-void draw()
+//functions
+string * setup()
 {
+    string player_1, player_2;
 
+    cout << "Please insert your name player 1: ";
+    cin >> player_1;
+
+    cout << "Please insert your name player 2: ";
+    cin >> player_2;
+
+    string * names = new string[2];
+    names[0] = player_1;
+    names[1] = player_2;
+
+    return names;
 }
 
-void setup()
+void draw()
 {
+    system("clear");
+    /*
+    12 * 12
+    lines at pos 4
 
+        |   |   
+    ------------
+        |   |   
+    ------------
+        |   |   
+    
+        
+    */
+    for (size_t i = 0; i < (2* dimension) -1; i++)
+    {
+        if (i % 2 == 0)
+            cout << "   |   |   " << endl; 
+        
+        else
+        {
+            for (size_t i = 0; i < box_size; i++)
+                cout <<"-"; 
+            cout << endl;
+        }
+        
+    }
+    cout << endl;
+
+
+    
 }
 
 void play()
@@ -29,9 +74,18 @@ int main()
 {
     if (!game_over)
     {
-        draw();
-        setup();
+        string * names  = setup();
+        cout << names[0] << endl;
+        //draw();
+    }
+    /*
+    while (!game_over)
+    {
         play();
     }
+    */
+    
 
+
+    return 0;
 }
